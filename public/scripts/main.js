2ec6fcc8b5ef5dc3e2c5fe37400e81fa895156a4
felippym <cutelariadoisaias@gmail.com>
2022-08-28 16:47:12 -0300

    modal cancel button + innerhtml h2, p, button changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,23 @@
-import { Modal } from "./modal";
+import Modal from "./modal.js";
 
 const modal = Modal();
 
-const checkButtons = document.querySelectorAll("actions a.check");
+const checkButtons = document.querySelectorAll(".actions a.check");
 
 checkButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     modal.open();
   });
 });
+
+// botao de cancelar
+
+const cancelmodal = document.querySelectorAll(".modal .button");
+
+cancelmodal.forEach((button) => {
+  button.addEventListener("click", (evento) => {
+    modal.close();
+  });
+});
+
+const deleteButtons = document.querySelectorAll(".action a.delete");
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,9 +1,26 @@
-module.exports = function Modal() {
-  function open() {}
-  function close() {}
+export default function Modal() {
+  function open() {
+    document.querySelector(".modal-wrapper h2").innerHTML = "Marcar como lida";
+
+    document.querySelector(".modal-wrapper p").innerHTML =
+      "Tem certeza que você deseja marcar esta pergunta como lida?";
+    document.querySelector(".modal-wrapper button").innerHTML =
+      "Sim, marcar como lida";
+
+    document.querySelector(".modal-wrapper").classList.add("active");
+  }
+  function close() {
+    document.querySelector(".modal-wrapper").classList.remove("active");
+  }
+
+  function deleteOption() {}
+
+  function checkOption() {}
 
   return {
     open,
     close,
+    deleteOption,
+    checkOption,
   };
-};
+}
