50b36249109af0f02590521fe008d2d4bd32d18f
felippym <cutelariadoisaias@gmail.com>
2022-08-30 11:51:11 -0300

    sala sendo criada apenas com senha em numeros

--- src/route.js
+++ src/route.js
@@ -1,9 +1,22 @@
 const express = require("express");
-
+const questionController = require("./controllers/QuestionController");
 const route = express.Router();
+const RoomController = require("./controllers/RoomController");
+const Database = require("./db/config");
 
-route.get("/", (req, res) => res.render("index"));
-route.get("/room", (req, res) => res.render("room"));
-route.get("/create-room", (req, res) => res.render("create-room"));
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-room", (req, res) =>
+  res.render("index", { page: "create-room" })
+);
+route.get("/room/:room", async (req, res) => {
+  const roomid = req.params.room;
+  res.render("room", { roomid });
+});
+
+//formato de como as infos devem vir do modalrgrgrggr
+route.post("/create-room", RoomController.create);
+
+route.post("question/create/:room", questionController.create);
+route.post("question/room/:room/:question/:action", questionController.index);
 
 module.exports = route;
